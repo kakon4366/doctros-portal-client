538778107eb9cedc265c64ff01a44f634a351f8a
kakon4366 <kakonbarman4366@gmail.com>
2022-05-10 13:47:50 +0600

    Header component added

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,75 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<div className="navbar bg-base-100">
+			<div className="navbar-start">
+				<div className="dropdown">
+					<label tabindex="0" className="btn btn-ghost lg:hidden">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-5 w-5"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M4 6h16M4 12h8m-8 6h16"
+							/>
+						</svg>
+					</label>
+					<ul
+						tabindex="0"
+						className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+					>
+						<li>
+							<a href="/home">Home</a>
+						</li>
+						<li>
+							<a href="/about">About</a>
+						</li>
+						<li>
+							<a href="/appointment">Appointment</a>
+						</li>
+						<li>
+							<a href="/reviews">Reviews</a>
+						</li>
+						<li>
+							<a href="/contact-Us">Contact Us</a>
+						</li>
+					</ul>
+				</div>
+				<a href="/" className="btn btn-ghost normal-case text-xl">
+					Doctors Portal
+				</a>
+			</div>
+			<div className="navbar-center hidden lg:flex">
+				<ul className="menu menu-horizontal p-0">
+					<li>
+						<a href="/home">Home</a>
+					</li>
+					<li>
+						<a href="/about">About</a>
+					</li>
+					<li>
+						<a href="/appointment">Appointment</a>
+					</li>
+					<li>
+						<a href="/reviews">Reviews</a>
+					</li>
+					<li>
+						<a href="/contact-Us">Contact Us</a>
+					</li>
+				</ul>
+			</div>
+			<div className="navbar-end">
+				<a href="/login">Login</a>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
