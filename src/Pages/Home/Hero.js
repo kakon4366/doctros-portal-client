5b55e038d100eb0c170b2b8d1c32ef64cb9ee8cb
kakon4366 <kakonbarman4366@gmail.com>
2022-05-11 12:26:06 +0600

    Hero section fix

--- src/Pages/Home/Hero.js
+++ src/Pages/Home/Hero.js
@@ -4,28 +4,30 @@ import PrimaryButton from "../../Components/PrimaryButton";
 
 const Hero = () => {
 	return (
-		<div className="hero min-h-screen bg-base-200">
-			<div className="hero-content flex-col lg:flex-row-reverse">
-				<div className="flex-1">
-					<img
-						src={chair}
-						alt=""
-						className="sm:max-w-sm md:max-w-md mx-auto lg:max-w-lg rounded-lg shadow-2xl"
-					/>
-				</div>
-				<div className="flex-1 mt-10 lg:mt-0">
-					<h1 className="text-5xl font-bold">
-						Your New Smile Starts Here
-					</h1>
-					<p className="py-6">
-						Lorem Ipsum is simply dummy text of the printing and
-						typesetting industry. Lorem Ipsum has been the industry's
-						standard dummy text ever since the
-					</p>
-					<PrimaryButton>Get Started </PrimaryButton>
+		<section>
+			<div className="hero min-h-screen bg-base-200">
+				<div className="hero-content flex-col lg:flex-row-reverse">
+					<div className="flex-1">
+						<img
+							src={chair}
+							alt=""
+							className="sm:max-w-sm md:max-w-md mx-auto lg:max-w-lg rounded-lg shadow-2xl"
+						/>
+					</div>
+					<div className="flex-1 mt-10 lg:mt-0">
+						<h1 className="text-5xl font-bold leading-snug">
+							Your New Smile Starts Here
+						</h1>
+						<p className="py-6">
+							Lorem Ipsum is simply dummy text of the printing and
+							typesetting industry. Lorem Ipsum has been the industry's
+							standard dummy text ever since the
+						</p>
+						<PrimaryButton>Get Started</PrimaryButton>
+					</div>
 				</div>
 			</div>
-		</div>
+		</section>
 	);
 };
 
